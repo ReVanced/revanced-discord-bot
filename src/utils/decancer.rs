3f05f8cd922e30755cc5a99a825c433bc4958e80
oSumAtrIX <johan.melkonyan1@web.de>
2022-08-21 02:30:39 +0200

    feat: allow ascii only names

--- src/utils/decancer.rs
+++ src/utils/decancer.rs
@@ -6,19 +6,25 @@ use super::*;
 const DECANCER: Decancer = Decancer::new();
 
 pub async fn cure(ctx: &serenity::Context, member: &serenity::Member) {
-    let display_name = member.display_name();
-    let name = display_name.to_string();
+    println!("Curing {}", member.display_name());
+    let name = member.display_name().to_string();
 
-    let cured_user_name = DECANCER.cure(&name);
+    let mut cured_name = DECANCER
+        .cure(&name)
+        .replace(|c: char| !(c == ' ' || c.is_ascii_alphanumeric()), "");
 
-    if name.to_lowercase() == cured_user_name {
+    if cured_name.len() == 0 {
+        cured_name = "ReVanced user" .to_string();
+    }
+
+    if name.to_lowercase() == cured_name {
         return; // username is already cured
     }
 
     match member
         .guild_id
         .edit_member(&ctx.http, member.user.id, |edit_member| {
-            edit_member.nickname(cured_user_name)
+            edit_member.nickname(cured_name)
         })
         .await
     {
