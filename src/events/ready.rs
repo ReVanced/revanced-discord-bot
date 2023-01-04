503e095bd0dd3d9a780a8ff9fdf19d0935e24ddf
Ax333l <main@axelen.xyz>
2023-01-04 14:53:50 +0100

    fix(moderation): properly schedule unmutes

--- src/events/ready.rs
+++ src/events/ready.rs
@@ -1,5 +1,4 @@
 use chrono::Utc;
-use tracing::trace;
 
 use super::*;
 use crate::db::model::Muted;
@@ -24,34 +23,24 @@ pub async fn load_muted_members(ctx: &serenity::Context, _: &serenity::Ready) {
         .await
         .unwrap();
 
-    let http_ref = &ctx.http;
-
     while cursor.advance().await.unwrap() {
         let current: Muted = cursor.deserialize_current().unwrap();
         let Some(expires) = current.expires else { continue };
         let guild_id = current.guild_id.unwrap().parse::<u64>().unwrap();
-        let member_id = current.user_id.unwrap().parse::<u64>().unwrap();
+        let user_id = current.user_id.unwrap().parse::<u64>().unwrap();
 
-        if let Ok(guild) = http_ref.get_guild(guild_id).await {
-            if let Ok(member) = guild.member(http_ref, member_id).await {
-                let amount_left = std::cmp::max(expires as i64 - Utc::now().timestamp(), 0);
+        let amount_left = std::cmp::max(expires as i64 - Utc::now().timestamp(), 0);
 
-                data.pending_unmutes.insert(
-                    member.user.id.0,
-                    queue_unmute_member(
-                        ctx.clone(),
-                        data.database.clone(),
-                        member.guild_id,
-                        member.user.id,
-                        mute_role_id,
-                        amount_left as u64, // i64 as u64 is handled properly here
-                    ),
-                );
-            } else {
-                trace!("Failed to find member {} in guild {}", member_id, guild_id);
-            }
-        } else {
-            trace!("Guild {} unavailable", guild_id);
-        }
+        data.pending_unmutes.insert(
+            user_id,
+            queue_unmute_member(
+                ctx.clone(),
+                data.database.clone(),
+                serenity::GuildId(guild_id),
+                serenity::UserId(user_id),
+                mute_role_id,
+                amount_left as u64, // i64 as u64 is handled properly here
+            ),
+        );
     }
 }
