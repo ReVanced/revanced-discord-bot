56d690a099c480c35c1033999e33c25a8a2a6c6b
Ushie <ushiekane@gmail.com>
2023-01-15 15:47:15 +0300

    feat: add check for archive and lock status

--- src/events/thread_update.rs
+++ src/events/thread_update.rs
@@ -1,9 +1,13 @@
+use poise::serenity_prelude::ThreadMetadata;
+
 use crate::utils::keep_thread_alive::handle_keep_thread_alive;
 
 use super::*;
 
 pub async fn thread_update(ctx: &serenity::Context, thread: &serenity::GuildChannel) {
-    if let Err(why) = handle_keep_thread_alive(ctx, thread).await {
-        error!("Error unarchiving thread: {:?}", why);
+    if matches!(thread.thread_metadata, Some(ThreadMetadata { archived: true, locked: false, .. })) {
+        if let Err(why) = handle_keep_thread_alive(ctx, thread).await {
+            error!("Error unarchiving thread: {:?}", why);
+        }
     }
 }
