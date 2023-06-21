67a4c963e08c319144a7584460c0c21ea7578444
oSumAtrIX <johan.melkonyan1@web.de>
2023-06-21 21:17:51 +0200

    perf: run message handlers concurrently

--- src/events/message_create.rs
+++ src/events/message_create.rs
@@ -1,7 +1,7 @@
 use super::*;
-use crate::utils::message_response::handle_message_response;
 use crate::utils::code_embed::utils::code_preview;
 use crate::utils::media_channel::handle_media_channel;
+use crate::utils::message_response::handle_message_response;
 
 pub async fn message_create(ctx: &serenity::Context, new_message: &serenity::Message) {
     let is_media_channel = handle_media_channel(ctx, new_message).await;
@@ -10,7 +10,8 @@ pub async fn message_create(ctx: &serenity::Context, new_message: &serenity::Mes
         return;
     };
 
-    handle_message_response(ctx, new_message).await;
-
-    code_preview(ctx, new_message).await;
+    tokio::join!(
+        handle_message_response(ctx, new_message),
+        code_preview(ctx, new_message)
+    );
 }
