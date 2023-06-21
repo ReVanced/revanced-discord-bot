26c3c5fedc14c60e9a14c3f29af3da7c2ff66847
oSumAtrIX <johan.melkonyan1@web.de>
2023-06-21 21:38:43 +0200

    feat: remove media channels

--- src/events/message_create.rs
+++ src/events/message_create.rs
@@ -1,15 +1,8 @@
 use super::*;
 use crate::utils::code_embed::utils::code_preview;
-use crate::utils::media_channel::handle_media_channel;
 use crate::utils::message_response::handle_message_response;
 
 pub async fn message_create(ctx: &serenity::Context, new_message: &serenity::Message) {
-    let is_media_channel = handle_media_channel(ctx, new_message).await;
-
-    if is_media_channel {
-        return;
-    };
-
     tokio::join!(
         handle_message_response(ctx, new_message),
         code_preview(ctx, new_message)
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,13 +1,12 @@
 use chrono::Duration;
 use poise::serenity_prelude::{self as serenity, Member, RoleId};
 
-pub mod message_response;
 pub mod bot;
 pub mod code_embed;
 pub mod decancer;
-pub mod message;
 pub mod macros;
-pub mod media_channel;
+pub mod message;
+pub mod message_response;
 pub mod moderation;
 pub mod poll;
 
